b5849207191e9d019194e984a18266394edfa9be
Denis Gulev <denis_13@hotmail.it>
2025-02-19 10:11:33 +0100

    WEEK-11: refactor app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import request, g
-from flask_cors import CORS, cross_origin
-import os
+from flask_cors import cross_origin
 
 from services.users_short import *
 from services.home_activities import *
@@ -17,102 +16,44 @@ from services.show_activity import *
 from services.update_profile import *
 
 from lib.cognito_token import jwt_required
-
-# HONEYCOMB
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
-# CLOUDWATCH (watchtower)
-import watchtower
-import logging
-from time import strftime
-
-# ROLLBAR
-import rollbar
-import rollbar.contrib.flask
-from flask import got_request_exception
-
-# HONEYCOMB
-# Initialize tracing and an exporter that can send data to Honeycomb
-# provider = TracerProvider()
-# processor = BatchSpanProcessor(OTLPSpanExporter())
-# provider.add_span_processor(processor)
-# trace.set_tracer_provider(provider)
-# tracer = trace.get_tracer(__name__)
-
-# CLOUDWATCH
-
-# Configuring Logger to Use CloudWatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
-LOGGER.info("***testing logs***")
+from lib.rollbar import init_rollbar
+from lib.honeycomb import init_honeycomb
+from lib.cors import init_cors
+from lib.cloudwatch import init_cloudwatch
 
 app = Flask(__name__)
 
-# ROLLBAR
+## initialization
+init_honeycomb(app)
 with app.app_context():
-  rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-  """init rollbar module"""
-  rollbar.init(
-    # access token
-    rollbar_access_token,
-    # environment name - any string, like 'production' or 'development'
-    'flasktest',
-    # server root directory, makes tracebacks prettier
-    root=os.path.dirname(os.path.realpath(__file__)),
-    # flask already sets up logging
-    allow_logging_basic_config=False)
+  init_rollbar()
+  init_cors(app)
 
-  # send exceptions from `app` to rollbar, using flask's signal system.
-  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-
-# Initialize automatic instrumentation with Flask
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
-
-frontend = os.getenv('FRONTEND_URL')
-backend = os.getenv('BACKEND_URL')
-origins = [frontend, backend]
-cors = CORS(
-  app,
-  resources={r"/api/*": {"origins": origins}},
-  headers=["Content-Type", "Authorization"],
-  expose_headers=["Authorization"],
-  methods=["OPTIONS,GET,HEAD,POST"]
-)
+def model_json(model):
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
 
 @app.route('/api/health-check')
 def health_check():
   return {'success': True, 'version': 1}, 200
-
 
 # @app.route('/rollbar/test')
 # def rollbar_test():
 #   rollbar.report_message('Hello World!', 'warning')
 #   return "Hello World!"
 
-@app.after_request
-def after_request(response):
-  timestamp = strftime('[%Y-%b-%d %H:%M]')
-  LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-  return response
+#CloudWatch logs
+# @app.after_request
+# def after_request(response):
+#   init_cloudwatch(response)
 
 @app.route("/api/message_groups", methods=['GET'])
 @jwt_required()
 def data_message_groups():
   model = MessageGroups.run(cognito_user_uuid=g.cognito_user_uuid)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  model_json(model)
 
 
 @app.route("/api/messages/<string:message_group_uuid>", methods=['GET'])
@@ -122,10 +63,7 @@ def data_messages(message_group_uuid):
     cognito_user_uuid=g.cognito_user_uuid,
     message_group_uuid=message_group_uuid
   )
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  model_json(model)
 
 @app.route("/api/profile/update", methods=['POST','OPTIONS'])
 @cross_origin()
@@ -139,10 +77,7 @@ def data_update_profile():
     bio=bio,
     display_name=display_name
   )
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  model_json(model)
 
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
@@ -167,10 +102,7 @@ def data_create_message():
       message_group_uuid=message_group_uuid
     )
 
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  model_json(model)
 
 def default_home_feed(e):
   app.logger.debug("unauthenticated request")
@@ -192,19 +124,13 @@ def data_notifications():
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 def data_handle(handle):
   model = UserActivities.run(handle)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  model_json(model)
 
 @app.route("/api/activities/search", methods=['GET'])
 def data_search():
   term = request.args.get('term')
   model = SearchActivities.run(term)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  model_json(model)
 
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
@@ -213,10 +139,7 @@ def data_activities():
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, g.cognito_user_uuid, ttl)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  model_json(model)
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
 def data_show_activity(activity_uuid):
@@ -229,10 +152,7 @@ def data_activities_reply(activity_uuid):
   user_handle  = 'denis'
   message = request.json['message']
   model = CreateReply.run(message, user_handle, activity_uuid)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  model_json(model)
 
 @app.route("/api/users/<string:handle>/short", methods=['GET'])
 def data_users_short(handle):
